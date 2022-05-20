4ea8ed7296899b8c301316a71607dff80f94bdb3
MUSTAFAA14 <105944758+MUSTAFAA14@users.noreply.github.com>
2022-05-20 20:45:35 +0200

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/MUSTAFAA14/3232131 >.
 #
 # This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+# Please see < https://github.com/MUSTAFAA14/3232131/blob/master/LICENSE >
 #
 # All rights reserved.
 
