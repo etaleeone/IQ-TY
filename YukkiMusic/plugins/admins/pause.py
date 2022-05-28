0c47cedd6d9b3388c2325426ebe23d936ddbd06c
MUSTAFAA14 <105944758+MUSTAFAA14@users.noreply.github.com>
2022-05-28 22:26:16 +0200

    Update pause.py

--- YukkiMusic/plugins/admins/pause.py
+++ YukkiMusic/plugins/admins/pause.py
@@ -9,7 +9,7 @@
 
 from pyrogram import filters
 from pyrogram.types import Message
-
+from strings.filters import command
 from config import BANNED_USERS
 from strings import get_command
 from YukkiMusic import app
@@ -22,7 +22,7 @@ PAUSE_COMMAND = get_command("PAUSE_COMMAND")
 
 
 @app.on_message(
-    filters.command(PAUSE_COMMAND)
+    command(PAUSE_COMMAND)
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
