b393fdfccc550ffc8feea17d17df1654b32c96a9
MUSTAFAA14 <105944758+MUSTAFAA14@users.noreply.github.com>
2022-05-28 22:28:30 +0200

    Update skip.py

--- YukkiMusic/plugins/admins/skip.py
+++ YukkiMusic/plugins/admins/skip.py
@@ -9,7 +9,7 @@
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
-
+from strings.filters import command
 import config
 from config import BANNED_USERS
 from strings import get_command
@@ -28,7 +28,7 @@ SKIP_COMMAND = get_command("SKIP_COMMAND")
 
 
 @app.on_message(
-    filters.command(SKIP_COMMAND)
+    command(SKIP_COMMAND)
     & filters.group
     & ~filters.edited
     & ~BANNED_USERS
