1e4f6765e6ad05a1fc243c4dae9a020be24d0c66
MUSTAFAA14 <105944758+MUSTAFAA14@users.noreply.github.com>
2022-05-31 04:33:28 +0200

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -35,6 +35,25 @@ from YukkiMusic.utils.inline.playlist import botplaylist_markup
 from YukkiMusic.utils.logger import play_logs
 from YukkiMusic.utils.stream.stream import stream
 
+force_btn = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                text="اشترك هنا", url="https://t.me/sasa_boody"
+            ),                        
+        ],        
+    ]
+)
+
+async def check_is_joined(message):    
+    try:
+        userid = message.from_user.id
+        status = await app.get_chat_member("Na_x3", userid)
+        return True
+    except Exception:
+        await message.reply_text("*انت لست مشترك في قناة البوت @sasa_boody ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+        return False
+      
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
 
